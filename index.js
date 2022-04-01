d8ddea66b4256aba7c1a9bbe0996328a650a50c0
Rakshan Thapar <rakshanthapar32@gmail.com>
2022-04-01 17:37:01 +0530

    Adding new Route and commenting cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
-
+//app.use(cors());
+app.get('/',(req,res)=>{
+    res.send("From Messaging Service hosted at 4001");
+});
 
 app.get('/messages',(req,res)=>{
     res.send("From Messaging Service hosted at 4001");
